1cc044a2fbd02d4bfdd3eb8801d0f3cfecebc673
s13998 <73510618+s13998@users.noreply.github.com>
2021-01-15 03:05:14 +0100

    Delete cwiczenia9 directory

--- cwiczenia9/Maybe.scala
+++
@@ -1,5 +0,0 @@
-trait Maybe[A] {
-  def applyFunction(f: A => Any) : Maybe[_]
-  def getOrElse[B](n:B) : Any
-
-}
--- cwiczenia9/No.scala
+++
@@ -1,8 +0,0 @@
-class No extends Maybe[Nothing] {
-  override def applyFunction(f: Nothing => Any): Maybe[_] = this
-
-  override def getOrElse[B](n: B): Any = n
-
-  override def toString: String = "No"
-
-}
